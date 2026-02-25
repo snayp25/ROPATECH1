804278781f4932bd0fe2f2b155b7bd2f023f4bd2
snayp25 <muradsultanov466@gmail.com>
2026-02-25 14:49:00 +0400

    s

--- script.js
+++ script.js
@@ -1,8 +1,37 @@
-  const burger = document.getElementById('burger');
-  const navList = document.getElementById('navList');
+  // const burger = document.getElementById('burger');
+  // const navList = document.getElementById('navList');
 
-  burger.addEventListener('click', () => {
-    burger.classList.toggle('active');
-    navList.classList.toggle('active');
+  // burger.addEventListener('click', () => {
+  //   burger.classList.toggle('active');
+  //   navList.classList.toggle('active');
+  // });
+
+let inputs = document.querySelectorAll('input');
+let values = [];
+let button = document.querySelector('button');
+function login() {
+    for (i = 0; i < inputs.length; i++){
+      values.push(inputs[i].value);
+      
+    }
+    console.log(values);
+    
+    let month = document.querySelector('#month');
+    if (month.value < 1 && month.value > 12){
+      let error = document.querySelector('.error');
+      error.innerHTML = 'Введите месяц от 1-12';
+    }
+  }
+  
+
+
+const element = document.getElementById('number');
+const CVV=document.getElementById('cvv');
+  IMask(element, {
+    mask: '0000 0000 0000 0000'
   });
+  IMask(CVV, {
+    mask: "000"
+  })
 
+button.addEventListener('click', login);
